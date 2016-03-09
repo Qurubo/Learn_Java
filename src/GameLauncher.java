3a56ce4c6fdd9e764c5e4463c85af3e0decf6e15
Kirill <qurubo@gmail.com>
2016-03-09 20:36:34 +0300

    Игра в угадывания

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -2,4 +2,9 @@
  * Created by qurub on 09.03.2016.
  */
 public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame g =new GuessGame();
+        g.startGame();
+    }
+
 }
--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,9 @@
  * Created by qurub on 09.03.2016.
  */
 public class Player {
+    int number = 0;
+    public void guess(){
+        number = (int) (Math.random()*10);
+        System.out.println("Думаю это число " + number);
+    }
 }
