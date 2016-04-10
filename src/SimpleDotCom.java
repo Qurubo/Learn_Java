f190e260989dc940b7937871aa7c3968c162e375
Kirill <qurubo@gmail.com>
2016-04-10 23:02:23 +0300

    Потопи сайт лайт версия

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -1,6 +1,26 @@
 public class SimpleDotCom {
-    public static void main(String[] args) {
+    int[] locationCells;
+    int numOfHits=0;
+    void setLocationCells(int[] locs){
+        locationCells =locs;
+    }
+    String checkYourself(String stringGuess){
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Мимо";
+        for(int cell:locationCells){
+            if(guess==cell){
+                result="Попал";
+                numOfHits++;
+                break;
 
+            }
+
+        }
+        if(numOfHits==locationCells.length){
+            result="Потопил";
+        }
+        System.out.println(result);
+        return result;
     }
 }
 
