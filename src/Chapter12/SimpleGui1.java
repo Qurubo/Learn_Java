87b72bc1de339c4fc6925513744933a24ba24e57
Kirill <qurubo@gmail.com>
2016-09-26 22:05:55 +0300

    Learn Gui

--- src/Chapter12/SimpleGui1.java
+++ src/Chapter12/SimpleGui1.java
@@ -1,8 +1,17 @@
 package Chapter12;
 
+import javax.swing.*;
+
 /**
  * Created by qurub on 26.09.2016.
  */
 public class SimpleGui1 {
-
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton btn = new JButton("Cleck me");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(btn);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
 }
