1e941d1890d9ab240fafb23477da4c42bc044a60
Kirill <qurubo@gmail.com>
2016-12-09 23:44:25 +0300

    Stepik_2: BigInteger

--- src/Stepik_2/BigInt.java
+++ src/Stepik_2/BigInt.java
@@ -1,7 +1,24 @@
 package Stepik_2;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 /**
  * Created by qurub on 09.12.2016.
  */
 public class BigInt {
+    public static void main(String[] args) {
+        int v;
+        Scanner scanner =new Scanner(System.in);
+        v=scanner.nextInt();
+        BigInteger a=factorial(v);
+        System.out.println(a.toString());
+    }
+    public static BigInteger factorial(int value) {
+        BigInteger a= BigInteger.valueOf(1);
+        for (int i = 1; i <=value; i++) {
+           a=a.multiply(BigInteger.valueOf(i));
+        }
+        return a; // your implementation here
+    }
 }
