12775bb848bb867e4ad57349bd8060dbb8162627
Kirill <qurubo@gmail.com>
2016-12-10 00:01:18 +0300

    Stepik_2: MergeArrays

--- src/Stepik_2/MergeArrays.java
+++ src/Stepik_2/MergeArrays.java
@@ -1,10 +1,34 @@
 package Stepik_2;
 
+import java.util.Arrays;
+
 /**
  * Created by qurub on 09.12.2016.
  */
 public class MergeArrays {
     public static void main(String[] args) {
-
+        assertEquals(mergeArrays(new int[]{0, 2, 4}, new int[]{1, 3, 5}), new int[]{0, 1, 2, 3, 4, 5}, "Test #1");
+        assertEquals(mergeArrays(new int[]{}, new int[]{1, 3, 5}), new int[]{1, 3, 5}, "Test #2");
+        assertEquals(mergeArrays(new int[]{}, new int[]{}), new int[]{}, "Test #3");
+        assertEquals(mergeArrays(new int[]{1, 1, 1}, new int[]{1, 1, 1}), new int[]{1, 1, 1, 1, 1, 1}, "Test #4");
+        assertEquals(mergeArrays(new int[]{1, 2, 3}, new int[]{1, 2, 3}), new int[]{1, 1, 2, 2, 3, 3}, "Test #5");
+        assertEquals(mergeArrays(new int[]{6}, new int[]{1, 3, 5}), new int[]{1, 3, 5, 6}, "Test #6");
+        assertEquals(mergeArrays(new int[]{6}, new int[]{5, 5, 5, 5}), new int[]{5, 5, 5, 5, 6}, "Test #7");
+        assertEquals(mergeArrays(new int[]{1, 2, 3, 5}, new int[]{6}), new int[]{1, 2, 3, 5, 6}, "Test #8");
+        assertEquals(mergeArrays(new int[]{1, 2, 3, 5}, new int[]{}), new int[]{1, 2, 3, 5}, "Test #9");
+        assertEquals(mergeArrays(new int[]{4, 4}, new int[]{1, 2, 3}), new int[]{1, 2, 3, 4, 4}, "Test #10");
+        assertEquals(mergeArrays(new int[]{1, 2, 3}, new int[]{4, 4}), new int[]{1, 2, 3, 4, 4}, "Test #11");
     }
+    public static int[] mergeArrays(int[] a1, int[] a2) {
+        int[] z={1,2,3,4,5};
+        return z; // your implementation here
+    }
+    public static void assertEquals(int[] value, int[] expected, String msg) {
+
+        if (!Arrays.equals(value, expected))
+            System.out.printf("%s: Error! Expected %s, got %s.\n", msg, Arrays.toString(expected), Arrays.toString(value));
+        else
+            System.out.printf("%s: Ok\n", msg);
+    }
+
 }
