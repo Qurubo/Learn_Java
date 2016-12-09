e1463e722a1dfdc6934cffc3396b693aed788976
Kirill <qurubo@gmail.com>
2016-12-10 00:41:42 +0300

    Stepik_2: MergeArrays

--- src/Stepik_2/MergeArrays.java
+++ src/Stepik_2/MergeArrays.java
@@ -15,12 +15,49 @@ public class MergeArrays {
         assertEquals(mergeArrays(new int[]{6}, new int[]{1, 3, 5}), new int[]{1, 3, 5, 6}, "Test #6");
         assertEquals(mergeArrays(new int[]{6}, new int[]{5, 5, 5, 5}), new int[]{5, 5, 5, 5, 6}, "Test #7");
         assertEquals(mergeArrays(new int[]{1, 2, 3, 5}, new int[]{6}), new int[]{1, 2, 3, 5, 6}, "Test #8");
-        assertEquals(mergeArrays(new int[]{1, 2, 3, 5}, new int[]{}), new int[]{1, 2, 3, 5}, "Test #9");
-        assertEquals(mergeArrays(new int[]{4, 4}, new int[]{1, 2, 3}), new int[]{1, 2, 3, 4, 4}, "Test #10");
+        assertEquals(mergeArrays(new int[]{}, new int[]{}), new int[]{}, "Test #9");
+        assertEquals(mergeArrays(new int[]{-3, -2, -1 , 0}, new int[]{-1, 1, 1}), new int[]{-3, -2, -1, -1, 0, 1, 1}, "Test #10");
         assertEquals(mergeArrays(new int[]{1, 2, 3}, new int[]{4, 4}), new int[]{1, 2, 3, 4, 4}, "Test #11");
     }
     public static int[] mergeArrays(int[] a1, int[] a2) {
-        int[] z={1,2,3,4,5};
+        int[] z=new int[a1.length+a2.length];
+//        for (int e = 0; e < a1.length; e++) {
+//            if (a1[e] < 0) {
+//                z[0+e]=a1[e];
+//            }else {
+//                z[z.length-1] = a1[e];
+//            }
+//            Arrays.sort(z);
+////            for (int i = 0; i < z.length-1; i++) {
+////                for (int j = 0; j < z.length-1; j++) {
+////                    if (z[j]>z[j+1]) {
+////                        int buf=z[j];
+////                        z[j]=z[j+1];
+////                        z[j+1]=buf;
+////                    }
+////                }
+////            }
+//        }
+//        for (int e = 0; e < a2.length; e++) {
+//            if (a2[e] < 0) {
+//                z[0+e]=a2[e];
+//            }else {
+//                z[z.length-1] = a2[e];
+//            }
+//            Arrays.sort(z);
+////            for (int i = 0; i < z.length-1; i++) {
+////                for (int j = 0; j < z.length-1; j++) {
+////                    if (z[j]>z[j+1]) {
+////                        int buf=z[j];
+////                        z[j]=z[j+1];
+////                        z[j+1]=buf;
+////                    }
+////                }
+////            }
+//        }
+        System.arraycopy(a1, 0, z, 0, a1.length);
+        System.arraycopy(a2, 0, z, a1.length, a2.length);
+        Arrays.sort(z);
         return z; // your implementation here
     }
     public static void assertEquals(int[] value, int[] expected, String msg) {
