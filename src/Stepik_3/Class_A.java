0d704cfeb3a793e0d83b6c971f401e1c4b112535
Kirill <qurubo@gmail.com>
2016-12-12 23:47:47 +0300

    Stepik_3: Inheritance

---
+++ src/Stepik_3/Class_A.java
@@ -0,0 +1,7 @@
+package Stepik_3;
+
+/**
+ * Created by qurub on 12.12.2016.
+ */
+public class Class_A {
+}
---
+++ src/Stepik_3/Class_B.java
@@ -0,0 +1,7 @@
+package Stepik_3;
+
+/**
+ * Created by qurub on 12.12.2016.
+ */
+public class Class_B extends Class_A {
+}
