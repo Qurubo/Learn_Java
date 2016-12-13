6caf94b5ac5463b0879a6cc32e21182e134ab45f
Kirill <qurubo@gmail.com>
2016-12-13 12:38:46 +0300

    Stepik_3: Figure

--- src/Stepik_3/Figure/Circle.java
+++ src/Stepik_3/Figure/Circle.java
@@ -3,5 +3,35 @@ package Stepik_3.Figure;
 /**
  * Created by qurub on 13.12.2016.
  */
-public class Circle {
+public class Circle extends Shape {
+    private final Point center;
+    private final double radius;
+
+    public Circle(Point point, double radius,Color color) {
+        super(color);
+        this.center = point;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI*radius*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle {"+
+                " center="+ center+
+                " radius="+radius+
+                " color="+getColor()+
+                "}";
+    }
 }
