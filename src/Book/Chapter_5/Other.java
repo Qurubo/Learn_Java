dade3b88a8062cca5576a292e6e5d10d8881e98f
Kirill <qurubo@gmail.com>
2017-02-08 20:43:40 +0300

    Abstract Classes

---
+++ src/Book/Chapter_5/Other.java
@@ -0,0 +1,7 @@
+package Book.Chapter_5;
+
+/**
+ * Created by qurub on 08.02.2017.
+ */
+public class Other {
+}
---
+++ src/Book/Other.java
@@ -0,0 +1,7 @@
+package Book;
+
+/**
+ * Created by qurub on 08.02.2017.
+ */
+public class Other {
+}
