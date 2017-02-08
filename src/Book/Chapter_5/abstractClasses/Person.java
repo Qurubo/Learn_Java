720dca96bf78f0b009bfbd077f3e0acbcc8f7c25
Kirill <qurubo@gmail.com>
2017-02-08 20:46:58 +0300

    Abstract Classes

--- src/Book/Chapter_5/abstractClasses/Person.java
+++ src/Book/Chapter_5/abstractClasses/Person.java
@@ -3,6 +3,15 @@ package Book.Chapter_5.abstractClasses;
 /**
  * Created by qurub on 08.02.2017.
  */
-public class Person {
-    
+public abstract class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+    public abstract String getDescription();
 }
---
+++ src/Book/Chapter_5/abstractClasses/PersonTest.java
@@ -0,0 +1,8 @@
+package Book.Chapter_5.abstractClasses;
+
+/**
+ * Created by qurub on 08.02.2017.
+ */
+public class PersonTest {
+
+}
