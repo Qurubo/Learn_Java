4323c6ed9640f55a8624468bba97601548ad8724
Kirill <qurubo@gmail.com>
2017-02-08 21:02:37 +0300

    Abstract Classes

---
+++ src/Book/Chapter_5/abstractClasses/Employee.java
@@ -0,0 +1,34 @@
+package Book.Chapter_5.abstractClasses;
+
+import java.time.LocalDate;
+
+/**
+ * Created by qurub on 08.02.2017.
+ */
+public class Employee extends Person {
+    private double salary;
+    private LocalDate hireday;
+
+    public Employee(String name, double salary, int year,int month, int day) {
+        super(name);
+        this.salary = salary;
+        hireday = LocalDate.of(year,month,day);
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireday() {
+        return hireday;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("an employee with a salary of $%.2f",salary);
+    }
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+}
